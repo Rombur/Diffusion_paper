d9b62db68c365415a8b3685dc31e9242e72b52b6
Jean C. Ragusa <jean.ragusa@tamu.edu>
2013-05-30 08:12:41 -0500

    random mesh for quads, output generated

--- small_codes/random_quad_mesh.m
+++ small_codes/random_quad_mesh.m
@@ -1,4 +1,5 @@
 clear ll; close all; clc
+% random mesh generated for quadrilaterals
 
 L=1;
 n=10;
@@ -19,6 +20,9 @@ end
 figure(1)
 surf(x,y,ones(n+1,n+1))
 view(0,-90)
+output_file1=strcat('random_quad_mesh_L',int2str(L),'_n',int2str(n));
+print('-dpdf',strcat(output_file,'.pdf'));
+print('-dpng',strcat(output_file,'.png'));
 
 % keep x=L/2 a straight line
 if(mod(n,2)==0)
@@ -28,3 +32,69 @@ if(mod(n,2)==0)
     surf(xx,y,ones(n+1,n+1))
     view(0,-90)
 end
+output_file2=strcat('random_quad_mesh_mid_x_L',int2str(L),'_n',int2str(n));
+print('-dpdf',strcat(output_file,'.pdf'));
+print('-dpng',strcat(output_file,'.png'));
+
+% keep x=L/2  and y=L/2 as straight lines
+if(mod(n,2)==0)
+    yy=y;
+    yy(:,n/2+1)=L/2;
+    figure(3)
+    surf(xx,yy,ones(n+1,n+1))
+    view(0,-90)
+end
+output_file3=strcat('random_quad_mesh_mid_xy_L',int2str(L),'_n',int2str(n));
+print('-dpdf',strcat(output_file,'.pdf'));
+print('-dpng',strcat(output_file,'.png'));
+
+%
+close all;
+matID=1;
+srcID=1;
+%%%%%%%%%%%%%%%%%%%%%
+output_file1=strcat(output_file1,'.txt')
+fid=fopen(output_file1,'w');
+fprintf(fid,'%s\n','polygon');
+n2=n*n;
+fprintf(fid,'%d\n',n2);
+for i=1:n
+    for j=1:n
+        fprintf(fid,'%d %g %g %g %g %g %g %g %g  %d %d \n',4,x(i,j)    ,y(i,j)    ,...
+                                                             x(i+1,j)  ,y(i+1,j)  ,...
+                                                             x(i+1,j+1),y(i+1,j+1),...
+                                                             x(i,j+1)  ,y(i,j+1)  ,matID,srcID);
+    end
+end 
+fclose(fid)
+%%%%%%%%%%%%%%%%%%%%%
+output_file2=strcat(output_file2,'.txt')
+fid=fopen(output_file2,'w');
+fprintf(fid,'%s\n','polygon');
+n2=n*n;
+fprintf(fid,'%d\n',n2);
+for i=1:n
+    for j=1:n
+        fprintf(fid,'%d %g %g %g %g %g %g %g %g  %d %d \n',4,xx(i,j)    ,y(i,j)    ,...
+                                                             xx(i+1,j)  ,y(i+1,j)  ,...
+                                                             xx(i+1,j+1),y(i+1,j+1),...
+                                                             xx(i,j+1)  ,y(i,j+1)  ,matID,srcID);
+    end
+end 
+fclose(fid)
+%%%%%%%%%%%%%%%%%%%%%
+output_file3=strcat(output_file3,'.txt')
+fid=fopen(output_file3,'w');
+fprintf(fid,'%s\n','polygon');
+n2=n*n;
+fprintf(fid,'%d\n',n2);
+for i=1:n
+    for j=1:n
+        fprintf(fid,'%d %g %g %g %g %g %g %g %g  %d %d \n',4,xx(i,j)    ,yy(i,j)    ,...
+                                                             xx(i+1,j)  ,yy(i+1,j)  ,...
+                                                             xx(i+1,j+1),yy(i+1,j+1),...
+                                                             xx(i,j+1)  ,yy(i,j+1)  ,matID,srcID);
+    end
+end 
+fclose(fid)
+
